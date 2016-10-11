169d17e2f6b3b415cb17a0e8c326e47b80c64fe9
HP_PC\HP <shott@mail.csuchico.edu>
2016-10-10 23:35:01 -0700

    2nd stage try username and password validation

--- form-validation.js
+++ form-validation.js
@@ -14,11 +14,17 @@ function validate(){
     }
     {
   var passEntered = document.getElementById("pass").value;
-  
+  if(passEntered.value.text "password"){
+    document.getElementById(passwordGroup).classList.add("has-error")
+  }
+  if(passEntered.value.text ""){
+    document.getElementById(passwordGroup).classList.add("has-success")
+  }
   //Show message that there is an error with the password...
   document.getElementById("passwordError").innerHTML="Bad password.";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
   document.getElementById("passwordGroup").classList.add("has-error");
+ }
 }
